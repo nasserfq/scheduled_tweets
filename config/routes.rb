340c3ac791546c74516e2a251d958fdd7e62e43f
nasserfq <nonq70007@gmail.com>
2021-05-03 16:51:01 +0300

    done with Part 17: Login Form

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,13 @@ Rails.application.routes.draw do
   #GET /about
   get "about", to: "about#index"
 
+  get "sign_up", to: "registrations#new"
+  post "sign_up", to: "registrations#create"
+
+  delete "logout", to: "sessions#destroy"
+
+  get "sign_in", to: "sessions#new"
+  post "sign_in", to: "sessions#create"
   # get "/", to: "main#index"
   root to: "main#index"
 end
